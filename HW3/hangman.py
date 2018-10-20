f106816550530c243d7bd0e6f5d97f454fa95eb7
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2018-10-20 22:04:06 +0300

    20.10.18 22:04
    Added task 3

---
+++ HW3/hangman.py
@@ -0,0 +1,10 @@
+from HW3.hangman_logic import *
+
+
+def main():
+    game = HangmanGame(4)
+    game.runGame()
+
+
+if __name__ == '__main__':
+    main()
---
+++ HW3/hangman_logic.py
@@ -0,0 +1,75 @@
+import random
+
+
+class HangmanGame():
+
+    def __init__(self, tryc):
+        self.maxtriescount = tryc
+        self.words = ['airfield', 'hatchet', 'holidays', 'computer', 'university', 'photoconduction']
+
+    def startNewGame(self):
+        self.currentword = self.words[random.randrange(len(self.words))]
+        self.currenttry = self.maxtriescount
+        self.openedChars = []
+
+    def openChar(self, char):
+        if self.currentword.find(char.lower()) != -1:
+            self.openedChars.append(char.lower())
+            return 1
+        else:
+            self.currenttry -= 1
+            return 0
+
+    def canUseChar(self, char):
+        return self.openedChars.count(char.lower()) == 0
+
+    def charToWrite(self, index):
+        if self.openedChars.count(self.currentword[index]) != 0:
+            return self.currentword[index]
+        else:
+            return '_'
+
+    def getCurState(self):
+
+        res = ''
+
+        for i in range(len(self.currentword)):
+            res += (self.charToWrite(i) + ' ')
+
+        return res
+
+    def checkWin(self):
+
+        res = ''
+
+        for i in range(len(self.currentword)):
+            res += (self.charToWrite(i))
+
+        if res == self.currentword:
+            return 1
+        else:
+            return 0
+
+    def checkLose(self):
+        return self.currenttry == 0
+
+    def runGame(self):
+
+        self.startNewGame()
+        print(self.getCurState())
+
+        while not self.checkWin() and not self.checkLose():
+
+            ch = input('Enter char: ')
+
+            while not self.canUseChar(ch):
+                ch = input('Enter char: ')
+
+            self.openChar(ch)
+
+            print(self.getCurState())
+
+        if self.checkWin():
+            print('You won!')
+        else:
+            print('You lose(')
