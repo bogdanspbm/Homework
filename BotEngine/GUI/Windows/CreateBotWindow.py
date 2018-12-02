1db8943a710bcda998ec58c3b8d7a4bd69140749
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2018-12-02 17:19:41 +0300

    31
    3

--- BotEngine/GUI/Windows/CreateBotWindow.py
+++ BotEngine/GUI/Windows/CreateBotWindow.py
@@ -7,6 +7,11 @@ class Child(tk.Toplevel):
         super().__init__(root)
         self.root = root
         self.enter_name = -1
+        self.image = tk.PhotoImage(file='../Sprites/Popup.png')
+        self.image_bl = tk.PhotoImage(file='../Sprites/ButtonL_0.png')
+        self.image_br = tk.PhotoImage(file='../Sprites/ButtonR_0.png')
+        self.image_bl_1 = tk.PhotoImage(file='../Sprites/ButtonL.png')
+        self.image_br_1 = tk.PhotoImage(file='../Sprites/ButtonR.png')
         self.btn_enter = -1
         self.init_child()
 
@@ -14,16 +19,15 @@ class Child(tk.Toplevel):
         self.title('Create Bot')
         self.geometry('400x200+400+300')
         self.resizable(False, False)
+        self.window = tk.Canvas(self,width=self.image.width(), height=self.image.height(),bd=0, highlightthickness=0)
+        self.window.pack(expand=True)
+        self.window.create_image(self.image.width()/2,self.image.height()/2,image=self.image)
+        self.window.create_image(100, 180, image=self.image_bl, tag='accept')
+        self.window.create_image(300, 180, image=self.image_br, tag='back')
 
-        label_desc = tk.Label(self, text='Enter bot Name')
-        label_desc.place(x=50, y=50)
+        self.enter_name = ttk.Entry(self.window,width=2)
+        self.enter_name.place(x=100,y=50)
 
-        self.enter_name = ttk.Entry(self)
-        self.enter_name.place(x=200, y=50)
-
-        self.btn_enter = ttk.Button(self, text='Accept',
-                                    command=self.create_bot)
-        self.btn_enter.place(x=100, y=80)
 
         # self.grab_set()
         self.focus_set()
